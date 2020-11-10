1e84042bb4ac78daac1c42a191e7ba95b81d33fe
TheoCorn <68430877+TheoCorn@users.noreply.github.com>
2020-11-10 10:54:15 +0100

    Add files via upload

--- bluetooth/Constants.kt
+++ bluetooth/Constants.kt
@@ -4,7 +4,7 @@ import com.tcorp.cap.btimpthread.BuildConfig
 
 enum class Constants(val string: String) {
     BLUETOOTH_SERVICE_NOTIFICATION_CHANEL("${BuildConfig.APPLICATION_ID}.btServiceNotification"),
-    NOTIFICATION_NAME("BLUETOOTH_SERVICE_NOTIFICATION_NAME"),
+    NOTIFICATION_NAME("BLUETOOTH_SERVICE"),
     UMD_IS_RUNNING_BLUETOOTH("Bluetooth"),
     IntentOnDisconnect("ON_DISCONNECT"),
     DeviceName("DEVICE_NAME")
