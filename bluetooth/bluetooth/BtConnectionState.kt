97fe323de942675b6f6b0f283887aad8d217e5fc
TheoCorn <68430877+TheoCorn@users.noreply.github.com>
2020-11-10 11:11:55 +0100

    Delete BtConnectionState.kt

--- bluetooth/bluetooth/BtConnectionState.kt
+++
@@ -1,8 +0,0 @@
-package com.tcorp.cap.bluetooth
-
-enum class BtConnectionState(val state: String) {
-    CONNECTED("connected"),
-    DISCONNECTED("disconnected"),
-    CONNECTING("connecting"),
-    NO_BLUETOOTH("no bluetooth available")
-}
